03e819ac86de9702073be2f5348195eeb440041a
Juha Laiho <juha.laiho@outlook.com>
2018-08-23 19:11:07 +0300

    Added StulTulos files

---
+++ StulConversions/StulFiles/StulTulosTxt/StulTulosWriter.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StulFiles.StulTulosTxt
+{
+    public class StulTulosWriter
+    {
+        private const string DefaultCsvDelimiter = ",";
+        private string TulosFileName { get; set; }
+        public IList<StulTulosLine> TulosLines { get; set; }
+
+        public StulTulosWriter(string tulosFileName)
+        {
+            TulosFileName = tulosFileName;
+            TulosLines = new List<StulTulosLine>();
+        }
+        public void WriteAll()
+        {
+            var outFileStream = System.IO.File.Create(TulosFileName);
+            using (StreamWriter stream = new StreamWriter(outFileStream, Encoding.UTF8))
+            {
+                var writer = new CsvWriter(stream);
+                writer.Configuration.Delimiter = DefaultCsvDelimiter;
+                writer.Configuration.QuoteAllFields = true;
+                writer.Configuration.RegisterClassMap<StulTulosLineClassMap>();
+
+                foreach (StulTulosLine tulos in TulosLines.OrderBy(t => t.CoupleNumber))
+                {
+                    writer.WriteRecord<StulTulosLine>(tulos);
+                    writer.NextRecord();
+                }
+            }
+        }
+    }
+}
