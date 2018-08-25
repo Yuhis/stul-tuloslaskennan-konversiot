4c029ee127142abbb54ee1060fc666985a89b909
Juha Laiho <juha.laiho@vincit.fi>
2018-08-25 23:49:37 +0300

    Added StulTulos GUI project

---
+++ StulConversions/StulTulos/Program.cs
@@ -0,0 +1,42 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+namespace StulTulos
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var app = new CommandLineApplication
+            {
+                Name = "StulTulos",
+                Description = "Konvertoi TPS results tiedoston STUL tulos muotoon"
+            };
+            app.HelpOption(inherited: true);
+
+            var optionParitTxt = app.Option("-p|--parit <parit.txt>", "STUL parit.txt tiedoston nimi", CommandOptionType.SingleValue)
+                .Accepts(v => v.ExistingFile());
+            var optionResultsXml = app.Option("-r|--results <results.xml>", "TPS results.xml tiedoston nimi", CommandOptionType.SingleValue)
+                .Accepts(v => v.ExistingFile());
+            var optionTulosTxt = app.Option("-t|--tulos <tulos.txt>", "STUL tulos.txt tiedoston nimi", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                var paritTiedosto = optionParitTxt.HasValue()
+                    ? optionParitTxt.Value()
+                    : "parit.txt";
+                var resultsTiedosto = optionResultsXml.HasValue()
+                    ? optionResultsXml.Value()
+                    : "results.xml";
+                var tulosTiedosto = optionTulosTxt.HasValue()
+                    ? optionTulosTxt.Value()
+                    : "tulos.txt";
+
+                var program = new TpsResultsToStulTulosConverter.TpsResultsToStulTulosConverter(paritTiedosto, resultsTiedosto, tulosTiedosto);
+                program.WriteTulosTxt();
+                return 1;
+            });
+
+            return app.Execute(args);
+        }
+    }
+}
